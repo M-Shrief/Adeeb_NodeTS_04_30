d33b300a9e04efdcb880562a0e9ba9ee4178a2b1
JustRollingDevs <justrollingdevs@protonmail.com>
2023-03-13 18:07:12 +0200

    added body-parser, cors and connected mongoose to DB

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,34 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import mongoose, { ConnectOptions } from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
 
-app.get("/", (req: Request, res: Response): void => {
-  res.json({ message: "Please Like the Video!" });
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    methods: 'GET',
+  })
+);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// const MONGO_DB = process.env.MONGO_DB as string;
+mongoose
+  .connect(process.env.MONGO_DB as string, {
+    dbName: process.env.DB_NAME,
+    // other option are no longer required for v6+
+  })
+  .then(() => console.log('connected'))
+  .catch((err) => console.log(err));
+
+app.get('/', (req: Request, res: Response) => {
+  res.json({ message: 'Please Like the Video!' });
 });
 
-app.listen("3001", (): void => {
-  console.log("Server Running!");
+app.listen('3000', (): void => {
+  console.log('Server Running!');
 });
